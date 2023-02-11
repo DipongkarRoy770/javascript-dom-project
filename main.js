252a9b1d6ea252e7652240fa9a7ce55eb3c67cf6
DipongkarRoy770 <dipongkarroy233@gmail.com>
2023-02-11 14:04:22 +0600

    done

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-1').addEventListener('click',function(){
+  const titles = document.getElementById('title');
+  titles.innerText ='Dhaka International university';
+   titles.style.color = 'red';
+   titles.style.fontWeight =800 ;
+})
+//funtion diya kaj kora:
+function clickMe(){
+  const titleMe = document.getElementById('title');
+  titleMe.innerText = 'Delta Computer Science College,Rangpur'
+  titleMe.style.color = 'orange'
+
+}
